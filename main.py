30d18b6479fb3a662d13e86f896db95fbfe94148
kself1875 <103849544+kself1875@users.noreply.github.com>
2022-09-30 23:34:31 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,85 @@
+# Generate an introduction for the chatbot
+# Acquire name from end-user
+# Print a greeting for the end-user name
+
+print('Hello, This is Mr. Ali T. That is First name: Mr., Middle name: Ali; Last name: T.')
+print('I will be your ChatBot today!')
+print('And I believe in the Golden Rule: The man with the gold...RULES!!!')
+print('Who am I speaking with today?!')
+name = input('My name is:')
+print('Greetings', name, ', It is nice of you to meet me sucka!')
+print('I cannot see you', name, ', But I bet you so ugly, when you cry the tears run and go down the back of your head!')
+
+# Feeling conversation
+print('How do you feel today?')
+
+# get feeling from end-user
+feeling = input('I feel:')
+print('Well, You should feel with your hands..Try it one day!')
+
+# Age conversation
+
+print('How old are you', name, ', ?')
+
+# get age from user
+user_age = input('I am :')
+print(user_age,'!', 'You so young I bet you think Vanilla Ice is the name of a ice cream flavor!')
+print(name, ' Let me give you some advice...')
+print('The person who views the world the same at 50, as he did at 20, has wasted 30 years of his life!')
+print('Can you dig it chump?')
+choice = (input('(y)Yes or (n)No:'))
+
+# conditional statements for user choice
+if choice == 'y':
+    print('Good.. It takes a smart man to play dumb!')
+elif choice == 'n':
+    print('I got no time for the jibba-jabba!')
+
+# Food conversation
+print('What type of food you like?')
+# get food information
+food = input('I like:')
+print('I like', food, 'also!')
+print('Let me tell you something, when I was a kid, I got three meals a day...')
+print('Oatmeal, miss-a-meal, and no meal!')
+print('I am so pretty... and I pity the FOOL!!')
+
+# Hobby conversation
+print('What do you like to do for fun:')
+
+# get hobby from end-user
+hobby = input('I like:')
+print(hobby, 'Not bad!..')
+print('I like this also.')
+print('If you ever dream of beating me.. You better wake up and apoloogize!')
+
+# Animal conversation
+
+print('What is your favorite animal?')
+
+# get favorite animal from end-user
+animal = input('My favorite animal is:')
+print(animal, 'Not so tough!')
+print('Listen... I have wrestled alligators...')
+print('I have tussled a whale...')
+print('Handcuffed lightning and threw thunder in jail!')
+print('Speaking of animals, Joe Frazier is so ugly he need to donate his face to the US Bureau of Wildlife!')
+
+# Goodbye
+print('Well', name, 'It was nice to meet you.')
+print('It was real...')
+print('It was fun...')
+print('But it was not real fun..')
+print('And remember...')
+print('Live everyday like it is your last because someday.....')
+print('You are going to be right!')
+
+
+
+
+
+
+
+
+
+
